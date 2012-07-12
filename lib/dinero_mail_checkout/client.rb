c7d6157df0100d1f9dcd4676d4fd1a7432805104
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-07-12 19:42:53 -0300

    created the get_uri method on client class

--- lib/dinero_mail_checkout/client.rb
+++ lib/dinero_mail_checkout/client.rb
@@ -1,5 +1,11 @@
+require 'uri'
+
 module DineroMailCheckout
   class Client
-
+    class << self
+      def get_uri(data)
+        "#{DineroMailCheckout::Configuration::CHECKOUT_URL}?#{URI.encode_www_form(DineroMailCheckout::CheckoutData.validate(data))}"
+      end
+    end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe DineroMailCheckout do
-
+  describe "validations" do
+    before do
+      DineroMailCheckout.configure do |config|
+        config.payment_method = 'all'
+        config.merchant = 'merchant'
+        config.logo_url = 'http://localhost:3000/assets/logo.png'
+        config.success_url = 'http://localhost:3000/success_page'
+        config.error_url = 'http://localhost:3000/error_page'
+        config.currency = DineroMailCheckout::Configuration::Currency::CLP
+      end
+    end
+    it "should redirect to DineroMail checkout page" do
+      data = {country_id: 2, item_name_1: "Name", item_quantity_1: 1, item_ammount_1: 10}
+      DineroMailCheckout::Client.get_uri(data).should =~ /checkout.dineromail.com\/CheckOut\?country_id=2&item_name_1=Name&item_quantity_1=1&item_ammount_1=10&merchant=merchant&payment_method_available=all&ok_url=http%3A%2F%2Flocalhost%3A3000%2Fsuccess_page&error_url=http%3A%2F%2Flocalhost%3A3000%2Ferror_page&currency=clp/
+    end
+  end
 end
